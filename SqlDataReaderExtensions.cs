e112cab85c081af54fd250fa8fac474938c07c3c
mohammed jouhari <m_243_4@hotmail.com>
2023-01-31 03:17:07 +0300

    first commit

---
+++ SqlDataReaderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+
+namespace Ado.NetSqlHelper
+{
+    public static class SqlDataReaderExtensions
+    {
+        public static IEnumerable<T> Select<T>(this IDataReader reader, Func<IDataReader, T> projection)
+        {
+            while (reader.Read())
+            {
+                yield return projection(reader);
+            }
+        }
+    }
+}
